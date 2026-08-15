87fad5faa1207a693a6175447c802eb4bec70b79
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:32 +0100

    add cpp04/ex03/incs/Ice.hpp

---
+++ cpp04/ex03/incs/Ice.hpp
@@ -0,0 +1,17 @@
+
+#ifndef ICE_HPP
+#define ICE_HPP
+
+#include <AMateria.hpp>
+
+class Ice : public AMateria {
+	public:
+		Ice();
+		Ice(const Ice& copy);
+Ice&	operator=(const Ice& copy);
+		~Ice();
+		AMateria*	clone()			const;
+		void		use(ICharacter& target);
+};
+
+#endif
