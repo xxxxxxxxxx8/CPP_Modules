6231034ff33738300fa0332968783eb3af906240
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:31 +0100

    add cpp04/ex00/WrongCat.cpp

---
+++ cpp04/ex00/WrongCat.cpp
@@ -0,0 +1,24 @@
+
+#include <WrongCat.hpp>
+
+WrongCat::WrongCat() : WrongAnimal("WrongCat") {
+	std::cout << "Default Constructor of WrongCat called\n";
+}
+
+WrongCat::WrongCat(const WrongCat& copy) : WrongAnimal(copy) {
+	std::cout << "Copy Constructor of WrongCat called\n";
+}
+
+WrongCat::~WrongCat() {
+	std::cout << "Copy Destructor of WrongCat called\n";
+}
+
+WrongCat&	WrongCat::operator=(const WrongCat& copy) {
+	std::cout << "Copy assignment operator overloaded of WrongCat called\n";
+	WrongAnimal::operator=(copy);
+	return *this;
+}
+
+void	WrongCat::makeSound() const {
+	std::cout << "WrongMeow\n";
+}
