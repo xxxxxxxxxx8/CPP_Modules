a0c41cf346b12741f427a366b2262947e6d9c9d2
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:31 +0100

    add cpp03/ex02/FragTrap.hpp

---
+++ cpp03/ex02/FragTrap.hpp
@@ -0,0 +1,18 @@
+
+#ifndef FRAG_TRAP_HPP
+# define FRAG_TRAP_HPP
+
+#include <ClapTrap.hpp>
+
+class FragTrap : public ClapTrap {
+	public:
+				FragTrap();
+				FragTrap(const FragTrap& copy);
+				FragTrap(const std::string& name);
+	FragTrap&	operator=(const FragTrap& copy);
+				~FragTrap();
+		void	attack(const std::string& target);
+		void	highFivesGuys(void);
+};
+
+#endif
