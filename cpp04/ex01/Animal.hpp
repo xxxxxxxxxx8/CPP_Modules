ca282055f6a2c1c0675c92ffe7593ce647cdff8d
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:31 +0100

    add cpp04/ex01/Animal.hpp

---
+++ cpp04/ex01/Animal.hpp
@@ -0,0 +1,22 @@
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal {
+	protected:
+		std::string type;
+	public:
+					Animal();
+					Animal(const std::string& name);
+					Animal(const Animal& copy);
+					virtual
+					~Animal();
+Animal&				operator=(const Animal& copy);
+const std::string&	getType() const;
+virtual
+void				makeSound() const;
+};
+
+#endif
