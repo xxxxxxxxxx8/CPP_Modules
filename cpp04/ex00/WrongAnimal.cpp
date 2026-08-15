1e13e6dae3e24663ee7babc0c3a82e2f8c094fe5
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:31 +0100

    add cpp04/ex00/WrongAnimal.cpp

---
+++ cpp04/ex00/WrongAnimal.cpp
@@ -0,0 +1,31 @@
+
+#include  <WrongAnimal.hpp>
+
+WrongAnimal::WrongAnimal() : type("WrongAnimal"){
+	std::cout << "Default Constructor of WrongAnimal called\n";
+}
+
+WrongAnimal::WrongAnimal(const std::string& name) : type(name){
+	std::cout << "Parametric Constructor of WrongAnimal called\n";
+}
+WrongAnimal::WrongAnimal(const WrongAnimal& copy) : type(copy.type) {
+	std::cout << "Copy Constructor of WrongAnimal called\n";
+}
+
+WrongAnimal::~WrongAnimal() {
+	std::cout << "Copy Destructor of WrongAnimal called\n";
+}
+
+WrongAnimal&	WrongAnimal::operator=(const WrongAnimal& copy) {
+	std::cout << "Copy assignment operator overloaded of WrongAnimal called\n";
+	type = copy.type;
+	return *this;
+}
+
+const std::string&	WrongAnimal::getType() const {
+	return type;
+}
+
+void	WrongAnimal::makeSound() const {
+	std::cout << "shouldnt know ur class was a game\n";
+}
