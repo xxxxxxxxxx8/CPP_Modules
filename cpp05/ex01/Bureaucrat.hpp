b2c15ff4d2927e3281182ad1513ec2523cf39c18
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:32 +0100

    add cpp05/ex01/Bureaucrat.hpp

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+class Form;
+
+class Bureaucrat {
+	private:
+		const std::string	name;
+		int					grade;
+	public:
+					Bureaucrat();
+					Bureaucrat(const Bureaucrat& copy);
+					Bureaucrat(const std::string& _name, int _grade);
+					~Bureaucrat();
+Bureaucrat&			operator=(const Bureaucrat& copy);
+const std::string&	getName()	const;
+int 				getGrade()	const;
+void				increment();
+void				decrement();
+void				setGrade(int _grade);
+void				signForm(Form& form)	const;
+
+		class GradeTooHighException : public std::exception {
+			public:
+			const char*		what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+			const char*		what() const throw();
+		};
+};
+
+std::ostream&	operator<<(std::ostream &out, const Bureaucrat& obj);
+
+#endif
