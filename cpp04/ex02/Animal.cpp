0521878b3708987595de0220393b94672691d3c2
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:32 +0100

    add cpp04/ex02/Animal.cpp

---
+++ cpp04/ex02/Animal.cpp
@@ -0,0 +1,27 @@
+
+#include  <Animal.hpp>
+
+Animal::Animal() : type("Animal"){
+	std::cout << "Default Constructor of Animal called\n";
+}
+
+Animal::Animal(const std::string& name) : type(name){
+	std::cout << "Parametric Constructor of Animal called\n";
+}
+Animal::Animal(const Animal& copy) : type(copy.type) {
+	std::cout << "Copy Constructor of Animal called\n";
+}
+
+Animal::~Animal() {
+	std::cout << "Destructor of Animal called\n";
+}
+
+Animal&	Animal::operator=(const Animal& copy) {
+	std::cout << "Copy assignment operator overloaded of Animal called\n";
+	type = copy.type;
+	return *this;
+}
+
+const std::string&	Animal::getType() const {
+	return type;
+}
