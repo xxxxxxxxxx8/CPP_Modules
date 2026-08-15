6f499ff9162d0108d23f913c2124a7535db6b084
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:32 +0100

    add cpp04/ex02/Cat.hpp

---
+++ cpp04/ex02/Cat.hpp
@@ -0,0 +1,19 @@
+
+#ifndef CAT_HPP
+# define CAT_HPP
+
+#include <Animal.hpp>
+#include <Brain.hpp>
+
+class Cat : public Animal {
+	private:
+		Brain* brain;
+	public:
+		Cat();
+		Cat(const Cat& copy);
+		~Cat();
+Cat&	operator=(const Cat& copy);
+void	makeSound() const;
+};
+
+#endif
