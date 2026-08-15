d7013825c47c3ccec094b319b254b52bba0722bf
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:32 +0100

    add cpp04/ex03/incs/Cure.hpp

---
+++ cpp04/ex03/incs/Cure.hpp
@@ -0,0 +1,17 @@
+
+#ifndef CURE_HPP
+# define CURE_HPP
+
+#include <AMateria.hpp>
+
+class Cure : public AMateria {
+	public:
+		Cure();
+		Cure(const Cure& copy);
+Cure&	operator=(const Cure& copy);
+		~Cure();
+		AMateria*	clone()			const;
+		void		use(ICharacter& target);
+};
+
+#endif
