1ff04994bb42af693d5efe9234136703502b3821
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:31 +0100

    add cpp03/ex03/ClapTrap.hpp

---
+++ cpp03/ex03/ClapTrap.hpp
@@ -0,0 +1,33 @@
+
+#ifndef CLAP_TRAP
+# define CLAP_TRAP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+	protected:
+		std::string		name;
+		int 			HitPoints;
+		int				EnergyPoints;
+		int				Attack_damage;
+	public:
+
+						ClapTrap(const std::string& name);
+						ClapTrap(const std::string& name, unsigned int Hp, unsigned int Ep, unsigned int Ad);
+						ClapTrap();
+						ClapTrap(const ClapTrap& copy);
+		ClapTrap&		operator=(const ClapTrap& copy);
+		virtual			~ClapTrap();
+
+		virtual
+		void			attack(const std::string& target);
+		void			takeDamage(unsigned int amount);
+		void			beRepaired(unsigned int amount);
+
+		const
+		std::string& 	getName()			const;
+		int				getAttackDamage()	const;
+};
+
+#endif
