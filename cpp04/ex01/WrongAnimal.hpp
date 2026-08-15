7a610e02e5c8f4140f481c1bec053bd068dfe643
xxxxxxxxxx8 <mohamedamintarza@gmail.com>
2026-08-15 12:54:32 +0100

    add cpp04/ex01/WrongAnimal.hpp

---
+++ cpp04/ex01/WrongAnimal.hpp
@@ -0,0 +1,21 @@
+
+#ifndef WrongAnimal_HPP
+# define WrongAnimal_HPP
+
+#include <iostream>
+
+class WrongAnimal {
+	protected:
+		std::string type;
+	public:
+					WrongAnimal();
+					WrongAnimal(const std::string& name);
+					WrongAnimal(const WrongAnimal& copy);
+					virtual
+					~WrongAnimal();
+WrongAnimal&		operator=(const WrongAnimal& copy);
+const std::string&	getType() const ;
+void				makeSound() const ;
+};
+
+#endif
